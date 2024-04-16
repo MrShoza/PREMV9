103eb53959fbaf5ab79b14b782d7853efe10a173
MrShoza <164552032+MrShoza@users.noreply.github.com>
2024-04-17 08:50:07 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,256 @@
+#DIBUAT OLEH LORD MILZX
+echo
+apt install ruby 
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZX "
+echo "  ______|_____|_|___\__/_\__●________________________ "
+echo
+gem install lolcat
+clear
+echo
+echo " [X] Google Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install google==3.0.0
+clear
+echo
+echo
+echo " [X] requests Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install requests
+clear
+echo
+echo " [X] Faker Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install Faker==5.0.0
+clear
+echo
+echo " [X] regex Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install regex==2023.8.8
+clear
+echo
+echo " [X] ipaddress Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install ipaddress==1.0.11.
+clear
+echo
+echo " [X] phonenumbers Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install phonenumbers==8.13.21
+clear
+echo
+echo " [X] requests==2.31.0 Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install requests==2.31.0
+clear
+echo
+echo " [X] openai Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install openai==0.28.1
+clear
+echo
+echo " [X] pythonping Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install pythonping==1.1.4
+clear
+echo
+echo " [X] os-sys Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install os-sys==2.1.4
+clear
+echo
+echo " [X] sockets Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install sockets==1.0.0
+clear
+echo
+echo " [X] colorama Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install colorama==0.4.6
+clear
+echo
+echo " [X] argparse Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install argparse==1.4.0
+clear
+echo
+echo " [X] bs4 Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install bs4==0.0.1
+clear
+echo
+echo " [X] googlesearch-python Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install googlesearch-python==1.2.3
+clear
+echo
+echo " [X] urllib3 Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install urllib3==2.0.5
+clear
+echo
+echo " [X] pyexiftool Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install pyexiftool==0.5.5
+clear
+echo
+echo " [X] pycryptodome Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install pycryptodome
+clear
+echo
+echo " [X] licensing Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install licensing
+clear
+echo
+echo " [X] webbrowser Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install webbrowser
+clear
+echo
+echo " [X] init Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install init
+clear
+echo
+echo " [X] wget Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install wget
+clear
+echo
+echo " [X] rich Belom Ke Install " | lolcat
+echo " _|_|_|                        _|                _|  _| " | lolcat  
+echo "   _|    _|_|_|      _|_|_|  _|_|_|_|    _|_|_|  _|  _| " | lolcat  
+echo "   _|    _|    _|  _|_|        _|      _|    _|  _|  _| " | lolcat  
+echo "   _|    _|    _|      _|_|    _|      _|    _|  _|  _| " | lolcat  
+echo " _|_|_|  _|    _|  _|_|_|        _|_|    _|_|_|  _|  _| " | lolcat
+echo
+pip install rich
+clear
+echo
+echo " _______               __                 __ __ " | lolcat
+echo " |   _   \.-----.----.|  |--.---.-.-----.|__|  |" | lolcat
+echo " |.  1   /|  -__|   _||     |  _  |__ --||  |  |" | lolcat
+echo " |.  _   \|_____|__|  |__|__|___._|_____||__|__|" | lolcat
+echo " |:  1    \                                     " | lolcat
+echo " |::.. .  /                                     " | lolcat
+echo " `-------'                                      " | lolcat
+echo " [V] Semua Module Berhasil di install " | lolcat
+echo "-----------------------------------------"
+echo " Next selanjutnya jalankan comand berigut " | lolcat
+echo " Contoh : python Lord.py " lolcat
+                                         
